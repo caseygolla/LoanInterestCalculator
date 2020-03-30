f1266057eda8c809927d1101ca7d666db31a5a69
Casey Golla <caseygolla@yahoo.com>
2020-03-29 20:32:24 -0400

    Added Loans.cs class.

---
+++ LoanInterestCalculator/Loans.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanInterestCalculator
+{
+    public class Loans
+    {
+        List<Loan> loans;
+
+        public void CalculateLoans()
+        {
+            foreach (Loan loan in loans)
+            {
+                loan.CalculateAmmoritazation();
+            }
+        }
+
+        public void Add(Loan loan)
+        {
+            loans.Add(loan);
+        }
+
+        public void PrintLoanAmmortizations()
+        {
+
+            foreach (Loan loan in loans)
+            {
+                loan.PrintAmortization();
+            }
+            //TODO: This is essentially a place holder for when 
+            //the output is needed.
+        }
+    }
+}
